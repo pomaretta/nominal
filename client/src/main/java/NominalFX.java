5440a7af6d935ad91ffa467120320185d8c383fc
Carlos Pomares <cpomaresp@gmail.com>
2021-04-22 07:27:44 +0200

    Added maven project.

---
+++ client/src/main/java/NominalFX.java
@@ -0,0 +1,26 @@
+// MAIN CLASS
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class NominalFX extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane,600,600);
+
+        stage.setScene(scene);
+        stage.setTitle("Nominal");
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
