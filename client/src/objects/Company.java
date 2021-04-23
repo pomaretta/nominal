b86eb2ce134d2c15a98b1256a656d5c3c3dfe119
ainarejos <ainarejos2@nigul.cide.es>
2021-04-23 18:50:31 +0200

    Added two objects to objects, company and employee.

---
+++ client/src/objects/Company.java
@@ -0,0 +1,15 @@
+import java.sql.Timestamp;
+
+public class company {
+    int id;
+    String cif;
+    String name;
+    String streetAddress;
+    String state;
+    int phoneNumber;
+    String currencyDigit;
+    String currencyName;
+    String socialSecurityId;
+}
+
+
