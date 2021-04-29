399338fa33ac20cb20c79420e49dfdb4a88ca897
Carlos Pomares <cpomaresp@gmail.com>
2021-04-29 20:20:03 +0200

    Added database configuration enums

---
+++ client/src/main/java/configuration/DatabaseReader.java
@@ -0,0 +1,67 @@
+package configuration;
+
+public enum DatabaseReader {
+
+    /**
+     *
+     * Enum of the different credentials used
+     * for set up default connection with DB.
+     *
+     */
+
+    NOMINAL {
+        @Override
+        public String getUser() {
+            return "USERNAME";
+        }
+        @Override
+        public String getPassword() {
+            return "PASSWORD";
+        }
+        @Override
+        public String getHostname() {
+            return "HOSTNAME";
+        }
+        @Override
+        public String getPort() {
+            return "PORT";
+        }
+        @Override
+        public String getDatabase() {
+            return "DATABASE";
+        }
+    },
+    AUTH {
+        @Override
+        public String getUser() {
+            return "USERNAME";
+        }
+        @Override
+        public String getPassword() {
+            return "PASSWORD";
+        }
+        @Override
+        public String getHostname() {
+            return "HOSTNAME";
+        }
+        @Override
+        public String getPort() {
+            return "PORT";
+        }
+        @Override
+        public String getDatabase() {
+            return "DATABASE";
+        }
+    };
+
+    public abstract String getUser();
+    public abstract String getPassword();
+    public abstract String getHostname();
+    public abstract String getPort();
+    public abstract String getDatabase();
+
+    public String getURL(){
+        return String.format("jdbc:mysql://%s:%s/%s",getHostname(),getPort(),getDatabase());
+    }
+
+}
