6519cf2bf2932f816265eb594f9e741a749d2bbd
ainarejos <ainarejos2@nigul.cide.es>
2021-05-09 11:55:34 +0200

    updateField done.

--- client/src/main/java/controllers/forms/CompanyForm.java
+++ client/src/main/java/controllers/forms/CompanyForm.java
@@ -12,6 +12,7 @@ package controllers.forms;
 
 import application.NominalFX;
 import common.company.Company;
+import controllers.HomeController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -27,11 +28,6 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class CompanyForm extends ViewController implements Initializable {
-    private ObservableList companyList = FXCollections.observableArrayList();
-    private ArrayList<Company> companies;
-    private ArrayList<Object> objectsList = new ArrayList<>();
-
-    private Cache cache = new Cache();
 
     @FXML
     private ComboBox companySelector;
@@ -50,40 +46,24 @@ public class CompanyForm extends ViewController implements Initializable {
     @FXML
     private TextField socialSecurityField;
 
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        try {
-            companies = new ArrayList<>(NominalFX.nominalAPI.getCompaniesMinimal());
-        } catch (SQLException throwables) {
-            //
-        }
-        for (Company c : companies){
-            companyList.add(c.getName());
-        }
-
-        companySelector.setItems(companyList);
-
-
     }
 
     @FXML
-    public void updateFields() throws SQLException {
-
-        if (cache.containsCompany(companies.get(companySelector.getSelectionModel().getSelectedIndex()).getId())){
-            idField.setText(String.valueOf(cache.getCompanyById(companies.get(companySelector.getSelectionModel().getSelectedIndex()).getId()).getId()));
-            nameField.setText(String.valueOf(cache.getCompanyById(companies.get(companySelector.getSelectionModel().getSelectedIndex()).getId()).getName()));
-            stateField.setText(String.valueOf(cache.getCompanyById(companies.get(companySelector.getSelectionModel().getSelectedIndex()).getId()).getState()));
-            cifField.setText(String.valueOf(cache.getCompanyById(companies.get(companySelector.getSelectionModel().getSelectedIndex()).getId()).getCif()));
-            streetAddressField.setText(String.valueOf(cache.getCompanyById(companies.get(companySelector.getSelectionModel().getSelectedIndex()).getId()).getStreetAddress()));
-            phoneNumberField.setText(String.valueOf(cache.getCompanyById(companies.get(companySelector.getSelectionModel().getSelectedIndex()).getId()).getPhoneNumber()));
-            socialSecurityField.setText(String.valueOf(cache.getCompanyById(companies.get(companySelector.getSelectionModel().getSelectedIndex()).getId()).getSocialSecurityId()));
-
-        } else {
-            cache.add(NominalFX.nominalAPI.getCompanyById(companies.get(companySelector.getSelectionModel().getSelectedIndex()).getId()), cache.getCompanies());
-            updateFields();
-        }
-
+    public void updateFields() {
+        HomeController controller = (HomeController) this.manager.getController();
+        idField.setText(String.valueOf(controller.getCurrentCompany().getId()));
+        nameField.setText(controller.getCurrentCompany().getName());
+        stateField.setText(controller.getCurrentCompany().getState());
+        cifField.setText(controller.getCurrentCompany().getCif());
+        streetAddressField.setText(controller.getCurrentCompany().getStreetAddress());
+        phoneNumberField.setText(controller.getCurrentCompany().getPhoneNumber());
+        socialSecurityField.setText(controller.getCurrentCompany().getSocialSecurityId());
     }
 
+    @Override
+    public void shouldUpdate() {
+        updateFields();
+    }
 }
