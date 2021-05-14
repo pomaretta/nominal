b53d83ac3f2871f1e3a0a0ae0dcfe26df7cac102
Carlos Pomares <cpomaresp@gmail.com>
2021-05-14 17:11:58 +0200

    Added auth queries.

--- client/src/main/java/persistence/auth/Statements.java
+++ client/src/main/java/persistence/auth/Statements.java
@@ -8,7 +8,16 @@ public enum Statements {
     SELECT_USER_BY_NAME("SELECT id FROM user WHERE name = ?"),
     
     SELECT_USER_BY_ID("SELECT u.id as `user` ,u.name as name,u.password as password,up.privilege as privilege FROM `user` u INNER JOIN user_privelege up ON u.id = up.`user` WHERE u.id = ? ORDER BY up.created_at DESC LIMIT 1"),
-    SELECT_PRIVILEGE_BY_ID("SELECT p.id as id, p.name as name FROM nominal_auth_dev.privilege p WHERE p.id = ?");
+    SELECT_PRIVILEGE_BY_ID("SELECT p.id as id, p.name as name FROM nominal_auth_dev.privilege p WHERE p.id = ?"),
+
+    SELECT_COMPANIES_BY_USER("SELECT uc.company as id FROM nominal_auth_dev.user_companies uc WHERE uc.`user` = ?"),
+
+    UPDATE_USER_PASSWORD("UPDATE nominal_auth_dev.`user` u SET u.password = ? WHERE u.id = ?"),
+
+    INSERT_NEW_USER("INSERT INTO nominal_auth_dev.`user` (name,password) VALUES (?,?)"),
+    INSERT_NEW_PRIVILEGE("INSERT INTO nominal_auth_dev.privilege (name) VALUES (?)"),
+    INSERT_NEW_USER_PRIVILEGE("INSERT INTO nominal_auth_dev.user_privelege (`user`,privilege) VALUES (?,?)"),
+    INSERT_NEW_USER_COMPANY("INSERT INTO nominal_auth_dev.user_companies (`user`,company) VALUES (?,?)");
 
     private final String query;
 
