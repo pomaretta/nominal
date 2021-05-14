dc9b055fa31c581a88a597afd07da1330755be8b
Carlos Pomares <cpomaresp@gmail.com>
2021-05-14 22:46:32 +0200

    Added users.sql

---
+++ server/database/users.sql
@@ -0,0 +1,3 @@
+INSERT INTO nominal_auth_dev.`privilege` VALUES ('2021-05-03 19:47:28',1,'admin'),('2021-05-03 19:47:33',2,'default');
+INSERT INTO nominal_auth_dev.`user` VALUES ('2021-04-30 13:16:48',1,'admin','21232f297a57a5a743894a0e4a801fc3'),('2021-05-14 15:58:11',2,'joshua','d1133275ee2118be63a577af759fc052'),('2021-05-14 16:13:08',3,'carlos','0e34c2c0281c15317c9efbb3d91baa8e'),('2021-05-14 16:18:02',4,'adan','b34f06e907567dfdc6d3c214d42fb9d9'),('2021-05-14 16:20:30',5,'carlos.pomares','dc599a9972fde3045dab59dbd1ae170b');
+INSERT INTO nominal_auth_dev.`user_privelege` VALUES (1,'2021-05-03 19:50:14',4,1),(1,'2021-05-03 20:56:01',5,1),(2,'2021-05-14 16:06:32',6,1),(3,'2021-05-14 16:14:24',7,1),(4,'2021-05-14 16:18:49',9,1),(5,'2021-05-14 16:20:30',10,2);
